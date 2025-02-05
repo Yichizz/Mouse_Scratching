364a235c6abd2a2fdbc93c2c6b9526d359dab8f8
Yichizz <zyc23451@outlook.com>
2025-02-05 13:39:44 +0800

    minor updates

--- main_scratching.py
+++ main_scratching.py
@@ -2,11 +2,11 @@
 from utils.scratch_detector import ScratchDetector
 
 video_path = '../Scratching_Mouse/videos/A-1.mp4'
-pretrained_weights = 'runs/pose/train/weights/best.pt'
+pretrained_weights = 'runs/pose/train3/weights/best.pt'
 save_path = './A-1.csv'
 
 def main(video_path, pretrained_weights, save_path):
-    detector = ScratchDetector(video_path, pretrained_weights, save_path, plot_prediction = False)
+    detector = ScratchDetector(video_path, pretrained_weights, save_path, plot_prediction = True)
     detector.process_video()
 
 if __name__ == '__main__':
